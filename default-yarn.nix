506beea787b62cde97236fd1a8bccc4052e4cfe8
Skylar-Ray O'Quinn <soraskymizu@gmail.com>
2022-01-28 13:34:57 +0000

    Change package expression to workspace expression

--- default-yarn.nix
+++ default-yarn.nix
@@ -1,7 +1,7 @@
 { pkgs ? <nixpkgs>, ...}:
 with (import (pkgs + "/pkgs/development/tools/yarn2nix-moretea/yarn2nix/default.nix") { });
 {
-  force-bridge = mkYarnPackage {
+  force-bridge = mkYarnWorkspace {
     name = "force-bridge";
     src = ./.;
     packageJSON = ./package.json;
